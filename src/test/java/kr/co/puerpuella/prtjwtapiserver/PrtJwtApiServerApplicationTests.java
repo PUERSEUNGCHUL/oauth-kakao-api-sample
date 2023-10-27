d6be94073bc68b94e7fc1a26eb45d6d52fc94f75
puer <lhhl1029@gmail.com>
2023-10-27 22:03:44 +0900

    JPA, H2 설정 추가

---
+++ src/test/java/kr/co/puerpuella/prtjwtapiserver/PrtJwtApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.puerpuella.prtjwtapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrtJwtApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
